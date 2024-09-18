2381e196bc756095459d69807b8ff270ed72ffd3
job moore <jobmurdan@ohtmail.com>
2024-09-17 20:07:37 -0700

    añado el formulario y el boton pero sin funcionalidad

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <form @sumbit.prevent="submitForm">
+    <div>
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email" required />
+    </div>
+
+    <div>
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password" v-model="password" required />
+    </div>
+    
+    <button type="submit">Iniciar Sesión</button>
+
+    
+    </form>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
